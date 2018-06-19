8ad174ea0af7fcac9e445de62a53150e5b25e4fe
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-19 14:21:34 +0200

    Written tests

--- src/uart_connection.hpp
+++ src/uart_connection.hpp
@@ -8,18 +8,17 @@
 #ifndef UART_COMM_HPP
 #define UART_COMM_HPP
 
-#include "wrap-hwlib.hpp"
 #include "queue.hpp"
 
 /**
  * @brief Used to select the UART controllers available on the Arduino Due.
- * 
+ *
  * Pins:
  * One   - 18 and 19
  * Two   - 16 and 17
  * Three - 14 and 15
  */
-enum class UARTController {ONE, TWO, THREE};
+enum class UARTController { ONE, TWO, THREE };
 
 /**
  * @brief Superclass for any UART connection, hardware or mock based.
@@ -27,140 +26,140 @@ enum class UARTController {ONE, TWO, THREE};
  *
  */
 class UARTConnection {
-public:
-  /**
-   * @brief Begin a UART connection.
-   * 
-   * This method initializes the selected USART (universal synchronous and asynchronous receiver-transmitter) controller located
-   * on the Arduino Due. By default, this method is called at object construction, but this can be disabled.
-   * 
-   */
-  virtual void begin() = 0;
+  public:
+    /**
+     * @brief Begin a UART connection.
+     *
+     * This method initializes the selected USART (universal synchronous and asynchronous receiver-transmitter) controller located
+     * on the Arduino Due. By default, this method is called at object construction, but this can be disabled.
+     *
+     */
+    virtual void begin() = 0;
 
-  /**
-   * @brief Check how many bytes are available to read.
-   * 
-   * By checking the size of the receive buffer, we determine if new data has arrived.
-   * 
-   * @return unsigned int Amount of bytes available to read.
-   */
-  virtual unsigned int available() = 0;
+    /**
+     * @brief Check how many bytes are available to read.
+     *
+     * By checking the size of the receive buffer, we determine if new data has arrived.
+     *
+     * @return unsigned int Amount of bytes available to read.
+     */
+    virtual unsigned int available() = 0;
 
-  /**
-   * @brief Enables the internal USART controller.
-   * 
-   */
-  virtual inline void enable() = 0;
+    /**
+     * @brief Enables the internal USART controller.
+     *
+     */
+    virtual inline void enable() = 0;
 
-  /**
-   * @brief Disables the internal USART controller.
-   * 
-   */
-  virtual inline void disable() = 0;
+    /**
+     * @brief Disables the internal USART controller.
+     *
+     */
+    virtual inline void disable() = 0;
 
-  /**
-   * @brief Send a single byte.
-   * 
-   * @param c Byte.
-   * @return true Byte send.
-   * @return false Byte has not been send, USART controller not initialized.
-   */
-  virtual bool send(const uint8_t c) = 0;
+    /**
+     * @brief Send a single byte.
+     *
+     * @param c Byte.
+     * @return true Byte send.
+     * @return false Byte has not been send, USART controller not initialized.
+     */
+    virtual bool send(const uint8_t c) = 0;
 
-  /**
-   * @brief Send a string.
-   * 
-   * @param str String.
-   * @return true String send.
-   * @return false String has not been send, USART controller not initialized.
-   */
-  virtual bool send(const uint8_t *str) = 0;
+    /**
+     * @brief Send a string.
+     *
+     * @param str String.
+     * @return true String send.
+     * @return false String has not been send, USART controller not initialized.
+     */
+    virtual bool send(const uint8_t *str) = 0;
 
-  /**
-   * @brief Send a string.
-   * 
-   * As we cannot static cast a const char* pointer to a const char* pointer, we overload the send function.
-   * Using reinterpreter cast would be a unfavourable solution, as the side effects are unkwown.
-   * 
-   * @param data String.
-   * @return true String has been send.
-   * @return false String has not been send, USART controller not initialized.
-   */
-  virtual bool send(const char *data) = 0;
+    /**
+     * @brief Send a string.
+     *
+     * As we cannot static cast a const char* pointer to a const char* pointer, we overload the send function.
+     * Using reinterpreter cast would be a unfavourable solution, as the side effects are unkwown.
+     *
+     * @param data String.
+     * @return true String has been send.
+     * @return false String has not been send, USART controller not initialized.
+     */
+    virtual bool send(const char *data) = 0;
 
-  /**
-   * @brief Send a array of bytes with a specified length.
-   * 
-   * @param data Array of bytes.
-   * @param length Length of array.
-   * @return true Array of bytes send.
-   * @return false Array of bytes has not been send, USART controller not initialized.
-   */
-  virtual bool send(const uint8_t* data, size_t length) = 0;
+    /**
+     * @brief Send a array of bytes with a specified length.
+     *
+     * @param data Array of bytes.
+     * @param length Length of array.
+     * @return true Array of bytes send.
+     * @return false Array of bytes has not been send, USART controller not initialized.
+     */
+    virtual bool send(const uint8_t *data, size_t length) = 0;
 
-  /**
-   * @brief Receive a single byte.
-   * 
-   * By popping the first element of the receive buffer, we return a received byte in a FIFO manner.
-   * 
-   * @return uint8_t Received byte.
-   */
-  virtual uint8_t receive() = 0;
+    /**
+     * @brief Receive a single byte.
+     *
+     * By popping the first element of the receive buffer, we return a received byte in a FIFO manner.
+     *
+     * @return uint8_t Received byte.
+     */
+    virtual uint8_t receive() = 0;
 
-  /**
-   * @brief Checks if the internal USART controller has been initialized.
-   * 
-   * @return true Internal USART controller is initialized.
-   * @return false Internal USART has not been initialized. See the begin() method.
-   */
-  virtual bool isInitialized() = 0;
+    /**
+     * @brief Checks if the internal USART controller has been initialized.
+     *
+     * @return true Internal USART controller is initialized.
+     * @return false Internal USART has not been initialized. See the begin() method.
+     */
+    virtual bool isInitialized() = 0;
 
-  /**
-   * @brief Send a single byte.
-   * 
-   * @param c Byte.
-   */
-  virtual void operator<<(const uint8_t c) = 0;
+    /**
+     * @brief Send a single byte.
+     *
+     * @param c Byte.
+     */
+    virtual void operator<<(const uint8_t c) = 0;
 
-  /**
-   * @brief Send a string.
-   * 
-   * As we want to easily send strings using the shifting operator, we need to declare a operator<<(const char* ).
-   * The use of a operator<<(const uint8_t* ) would be favourable, but unfortunately not possible.
-   * 
-   * @param str String (must be null terminated).
-   */
-  virtual void operator<<(const char *str) = 0;
+    /**
+     * @brief Send a string.
+     *
+     * As we want to easily send strings using the shifting operator, we need to declare a operator<<(const char* ).
+     * The use of a operator<<(const uint8_t* ) would be favourable, but unfortunately not possible.
+     *
+     * @param str String (must be null terminated).
+     */
+    virtual void operator<<(const char *str) = 0;
 
-  /**
-   * @brief Receive a byte.
-   * 
-   * @param c Byte.
-   */
-  virtual void operator>>(uint8_t &c) = 0;
+    /**
+     * @brief Receive a byte.
+     *
+     * @param c Byte.
+     */
+    virtual void operator>>(uint8_t &c) = 0;
 
-private:
-  /**
-   * @brief Checks if the USART controller reports that the transmitter is ready to send.
-   * 
-   * @return true Ready to send.
-   * @return false Not ready to send.
-   */
-  virtual inline bool txReady() = 0;
+  private:
+    /**
+     * @brief Checks if the USART controller reports that the transmitter is ready to send.
+     *
+     * @return true Ready to send.
+     * @return false Not ready to send.
+     */
+    virtual inline bool txReady() = 0;
 
-  /**
-   * @brief Send a byte of the serial connection.
-   *
-   * @param char Byte to send.
-   */
-  virtual void sendByte(const uint8_t &b) = 0;
+    /**
+     * @brief Send a byte of the serial connection.
+     *
+     * @param char Byte to send.
+     */
+    virtual void sendByte(const uint8_t &b) = 0;
 
-  /**
-   * @brief Receive a single byte by reading the US_RHR register.
-   * 
-   * @return char 
-   */
-  virtual inline uint8_t receiveByte() = 0;
+    /**
+     * @brief Receive a single byte by reading the US_RHR register.
+     *
+     * @return char
+     */
+    virtual inline uint8_t receiveByte() = 0;
 };
 
 #endif
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "../src/mock_uart.hpp"
 
-TEST_CASE( "Example Test Case" ) {
+TEST_CASE("Example Test Case") {
     REQUIRE(10 == 10);
+}
+
+TEST_CASE("Construct MockUART instance") {
+    MockUART uart(115200, UARTController::THREE, false);
+
+    REQUIRE(!uart.isInitialized());
+    REQUIRE(uart.available() == 0);
+
+    uart.begin();
+
+    REQUIRE(uart.isInitialized());
+    
+    REQUIRE(uart.available() == 1);
+    REQUIRE(uart.available() == 2);
+
+    REQUIRE(uart.receive() == 0xAA);
+    REQUIRE(uart.receive() == 0xAA);
+
+    REQUIRE(uart.receive() == 0);
 }

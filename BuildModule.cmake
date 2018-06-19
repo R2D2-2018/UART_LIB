ec78a6a2b0dca9b2c30f4b8b4aa0edd8dc95d96c
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-19 14:18:39 +0200

    Updated build files to latest version

--- BuildModule.cmake
+++ BuildModule.cmake
@@ -12,6 +12,13 @@ string (REGEX REPLACE ";" " " cxxflags "${cxxflags}")
 set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${cxxflags}")
 set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostartfiles -nostdlib -Wl,--defsym=STACK_SIZE=92160")
 
+set (sources ${sources}
+    src/main.cpp
+    src/wrap-hwlib.cpp
+    src/libc-stub.cpp
+    src/hardware_uart.cpp
+)
+
 # The target chip that is compiled for:
 include (${toolchain}/targets/arm32/cm3/atmel/sam3x/sam3x8e/sam3x8e.cmake)
 

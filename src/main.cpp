8502ac21d58706ae787bb5fd48ed7ca8369d2808
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-19 12:44:41 +0200

    Cleaned up example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,35 @@
 #include "wrap-hwlib.hpp"
-
 #include "uart_connection.hpp"
 
-
+/**
+ * @brief Example for using the UART library.
+ */
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
 
     namespace target = hwlib::target;
 
+    ///< Wait a litle bit of proper initialization.
     hwlib::wait_ms(500);
-
-    hwlib::cout << "Initialized transceiver" << hwlib::endl;
-
 
     ///< Initialize UART controller 3. Use a 115200 baudrate to send and receive (used by the uArm Swift Pro to send Gcode commands).
     UARTConnection conn(115200, UARTController::THREE);
-    hwlib::wait_ms(500);
 
     long startMsReceive = hwlib::now_us() / 1000;
     long startMsSend = hwlib::now_us() / 1000;
 
     while (true) {
+        ///< Send "Hello World" every 2.5 seconds.
         if ((hwlib::now_us() / 1000) - startMsSend > 2500) {
             startMsSend = hwlib::now_us() / 1000;
             conn << "Hello World!";
         }
 
+        ///< Check for new received data every 30 milliseconds.
         if (conn.available() > 0 && (hwlib::now_us() / 1000) - startMsReceive > 30) {
             startMsReceive = hwlib::now_us() / 1000;
 
+            ///< Print the received data.
             for (unsigned int i = 0; i < conn.available(); i++) {
                 hwlib::cout << conn.receive();
             }

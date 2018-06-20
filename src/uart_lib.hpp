dfa41edd32db58d562e57f8b06340f1272f6b3d0
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-20 13:20:01 +0200

    Include uart_connection.hpp on all build targets

--- src/uart_lib.hpp
+++ src/uart_lib.hpp
@@ -4,11 +4,11 @@
 #ifdef BMPTK_TARGET_arduino_due
 
 ///< Include dependencies
-#include "uart_connection.hpp"
 #include "hardware_uart.hpp"
 
 #endif
 
+#include "uart_connection.hpp"
 #include "mock_uart.hpp"
 
 #endif

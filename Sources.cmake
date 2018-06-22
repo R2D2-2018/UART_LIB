8b508fadf9cbc6696294d89fcff28505a3740ef6
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2018-06-22 15:35:12 +0200

    Removed empty uart_connection.cpp file

--- Sources.cmake
+++ Sources.cmake
@@ -8,9 +8,9 @@ include_directories (${hwlib}/library)
 set (catch ${build_environment}/libraries/Catch2)
 include_directories (${catch}/single_include)
 
+
 # Source Files:
 
 set (sources
-    src/uart_connection.cpp
     src/mock_uart.cpp
 )
